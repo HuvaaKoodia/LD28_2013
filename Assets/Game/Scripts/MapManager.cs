04b1655315a2ed88bb0cf2aeed63e57965a55f1a
Fagakebbo <antero.juutinen@gmail.com>
2013-12-14 10:56:42 +0200

    MapGenerator, MapObjects

---
+++ Assets/Game/Scripts/MapManager.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MapManager : MonoBehaviour 
+{
+	public Tile tilePrefab;
+	public TileObjects tileObjects;
+	public int gridX = 3;
+	public int gridY = 3;
+	public GameObject policeStationPrefab;
+	private List<Tile> tiles = new List<Tile>();
+	
+	// Use this for initialization
+	void Start () 
+	{		
+		GenerateGrid();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{	
+		GridSelection();
+	}
+	
+	void GenerateGrid()
+	{
+		for (int i = 0; i < gridY; i++)
+		{
+			for (int e = 0; e < gridX; e++)
+			{	
+				Tile go = Instantiate(tilePrefab, new Vector3(e*tilePrefab.size.width, 0, i*tilePrefab.size.height), Quaternion.identity) as Tile;
+				tiles.Add(go);				
+			}
+		}		
+		
+		tiles[0].tileObject = (GameObject)Instantiate(policeStationPrefab, Vector3.zero, Quaternion.identity);
+		int tempId = 0;
+		
+		foreach (Tile t in tiles)
+		{
+			t.id = tempId++;
+		}
+		
+	}
+	
+	void UpdateTiles()
+	{
+		foreach (Tile t in tiles)
+		{
+			t.UpdateObjects();
+		}
+	}
+	
+	void GridSelection()
+	{
+		RaycastHit hit = new RaycastHit();
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		
+		if(Physics.Raycast(ray, out hit, 10) && hit.transform.gameObject.layer == LayerMask.NameToLayer("Tile") )
+       	{			
+			Tile t = hit.transform.parent.GetComponent<Tile>();
+			
+			Debug.Log(tiles.Contains(t));
+			Debug.Log(t.id);
+       	}
+		
+	}
+	
+}
---
+++ Assets/Game/Scripts/Tile.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tile : MonoBehaviour 
+{
+	public int id;
+	public Rect size;
+	public Vector3 position;
+	public int maxObjects = 10;
+	public GameObject tileObject;
+	public ArrayList characters;
+	
+	// Use this for initialization
+	void Start () 
+	{
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{	
+	}
+	
+	public void UpdateObjects () 
+	{
+		
+	}
+}
