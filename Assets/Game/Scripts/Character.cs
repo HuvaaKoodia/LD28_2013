dd36e956e41bbd51c9c7f4df823e3f077be82c54
Fagakebbo <antero.juutinen@gmail.com>
2013-12-14 13:00:11 +0200

    Map

--- Assets/Game/Scripts/Character.cs
+++ Assets/Game/Scripts/Character.cs
@@ -1,12 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Character : MonoBehaviour {
+public class Character : MonoBehaviour
+{
 
 	// Use this for initialization
+	public GameObject characterModel;
 	void Start () 
 	{
-	
+		
 	}
 	
 	// Update is called once per frame
